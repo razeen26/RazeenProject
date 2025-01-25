4bd09333827d501936473cf3c1f5199f62a177d2
gamanullah <gamanullah@commvault.com>
2025-01-25 14:18:09 +0530

    updated

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL for the FastAPI backend
-const API_BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://razeenproject.onrender.com";
 
 /**
  * Upload a new image with metadata and status.
